8e1a801fa003e59798027307cebb387cf1f096ae
guy111a <40334314+guy111a@users.noreply.github.com>
2022-05-27 21:27:27 +0300

    Add files via upload

---
+++ 433mhzRX.ino
@@ -0,0 +1,39 @@
+#include <VirtualWire.h>
+
+const int led_pin = LED_BUILTIN;
+const int transmit_pin = 12;
+const int receive_pin = 11;
+const int transmit_en_pin = 3;
+
+void setup()
+{
+  delay(1000);
+  Serial.begin(9600);  // Debugging only
+  Serial.println("setup");
+
+  // Initialise the IO and ISR
+  vw_set_tx_pin(transmit_pin);
+  vw_set_rx_pin(receive_pin);
+  vw_set_ptt_pin(transmit_en_pin);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);  // Bits per sec
+
+  vw_rx_start();       // Start the receiver PLL running
+}
+
+void loop()
+{
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+  if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    
+    digitalWrite(led_pin, HIGH); // Flash a light to show received good message
+    Serial.print(map(buf[0], 0, 100, -50, 50));
+    Serial.print(',');
+    Serial.print(map(buf[1], 0, 100, -50, 50));
+    Serial.println();
+    digitalWrite(led_pin, LOW);
+  }
+}
